4aec155a0e86776c0b83024d5825192ee9e2a315
darkrho <darkrho@outlook.es>
2024-10-22 10:56:37 +0200

    ex3.1

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+
+const app = express()
+
+const entries = [
+  {
+    "id": "1",
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": "2",
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": "3",
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": "4",
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
+]
+
+app.get("/", (request, response) => {
+  response.send('<h1>Hello World<h1/>')
+})
+// GET -> api/persons
+app.get("/api/persons", (request, response) => {
+  response.send(JSON.stringify(entries))
+})
+
+
+
+// start server
+const PORT = 3002
+app.listen(PORT, () => { console.log(`listening on port ${PORT}`) })
