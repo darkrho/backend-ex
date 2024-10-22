534fc2ff3c50281fe3260079e81f97a00a7ad291
darkrho <darkrho@outlook.es>
2024-10-22 15:52:32 +0200

    ex3.3-3.6

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 
 const app = express()
+app.use(express.json())
 
-const entries = [
+let entries = [
   {
     "id": "1",
     "name": "Arto Hellas",
@@ -46,9 +47,58 @@ app.get("/info", (request, response) => {
 app.get("/api/persons", (request, response) => {
   response.send(JSON.stringify(entries))
 })
+// GET -> api/persons/id
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id
+  const contact = entries.filter(person => person.id === id)
+  if (contact.length === 0) {
+    return response.status(404).end()
+  } else {
+    response.json(contact)
+  }
+})
+// DELETE -> api/persons/id
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id
+  const contacts = entries.filter(person => person.id !== id)
+  entries = contacts
+  response.status(204).end()
+})
+// POST -> api/persons
+const createId = () => {
+  const id = Math.max(...entries.map(person => Number(person.id)))
+  return String(id + 1)
+}
+const nameExist = (contacts, name) => {
+  const nameFilter = contacts.filter((person) => person.name === name)
+  console.log(nameFilter.length)
+  if (nameFilter.length > 0) {
+    return true
+  }
+  return false
+}
+app.post("/api/persons", (request, response) => {
+  const body = request.body
+  const id = createId()
+
+  if (!body.name || !body.number) {
+    return response.status(404).json({
+      error: 'name or number is missing'
+    })
+  }
+
+  if (nameExist(entries, body.name)) {
+    return response.status(400).json({
+      error: "name must be unique"
+    })
+  }
 
 
+  const newContact = { ...body, id: id }
+  entries.push(newContact)
 
+  response.json(newContact)
+})
 // start server
 const PORT = 3002
 app.listen(PORT, () => { console.log(`listening on port ${PORT}`) })
